0ce8461bc837a02184e45a72241670493981100d
Tomer Klein <tomer.klein@gmail.com>
2022-11-10 01:12:17 +0200

    added http method for sending message

--- app/app.py
+++ app/app.py
@@ -14,8 +14,8 @@ from fastapi_mqtt.fastmqtt import FastMQTT
 from fastapi.templating import Jinja2Templates
 
 
-# os.environ['PYTHONIOENCODING'] = 'utf-8'
-# os.environ['LANG'] = 'C.UTF-8'
+os.environ['PYTHONIOENCODING'] = 'utf-8'
+os.environ['LANG'] = 'C.UTF-8'
 LOG_LEVEL = os.getenv("LOG_LEVEL")
 
 logger.remove()
@@ -34,7 +34,7 @@ mqtt_config.version = MQTTv311
 fast_mqtt = FastMQTT(config=mqtt_config)
 fast_mqtt.init_app(app)
 templates = Jinja2Templates(directory="templates/")
-
+webhook_base_url=config.get("Whatsapp","webhook.base_url")
 messenger = WhatsApp(config.get("Whatsapp","api.token"),  phone_number_id=str(config.get("Whatsapp","api.phone_id")))
 
 @fast_mqtt.on_connect()
@@ -49,18 +49,14 @@ async def message_to_topic(client, topic, payload, qos, properties):
         send_message(payload["message"],payload["recipient"])
     except Exception as e:
         logger.error("oh snap something went wrong. "+ e)
+
 @fast_mqtt.on_disconnect()
 def disconnect(client, packet, exc=None):
     logger.debug("Disconnected from {}".format(mqtt_config.host))
 
-@fast_mqtt.on_subscribe()
-def subscribe(client, mid, qos, properties):
-    print("subscribed", client, mid, qos, properties)
 
-
-@app.get("/")
+@app.get(webhook_base_url, include_in_schema=False)
 async def verify(request: Request):
-    logger.warning(request.query_params.get('hub.mode'))
     if request.query_params.get('hub.mode') == "subscribe" and request.query_params.get("hub.challenge"):
         if not request.query_params.get('hub.verify_token') == config.get("Whatsapp","webhook.token"): #os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
@@ -68,17 +64,17 @@ async def verify(request: Request):
     return "Hello world", 200
 
 
-@app.get("/disclaimer", response_class=HTMLResponse)
+@app.get("/disclaimer", response_class=HTMLResponse, include_in_schema=False)
 def home(request: Request):
     return templates.TemplateResponse('disclaimer.html', context={'request': request})
 
-
-
-@app.post('/', include_in_schema=False)
+@app.get("/message/{recipient}/{message}/{token}")
+def send(recipient: str, message: str, token: str,template :str = "",language :str = ""):
+    if token == config.get("Whatsapp","webhook.token"):
+        return send_message(message,recipient,template,language)
+@app.post(webhook_base_url, include_in_schema=False)
 async def webhook(request: Request):
     data = await request.json()
-    
-    logger.info("Received webhook data: {}".format(data))
     changed_field = messenger.changed_field(data)
     if changed_field == "messages":
         new_message = messenger.get_mobile(data)
@@ -86,76 +82,35 @@ async def webhook(request: Request):
             mobile = messenger.get_mobile(data)
             name = messenger.get_name(data)
             message_type = messenger.get_message_type(data)
-            logger.info(
-                f"New Message; sender:{mobile} name:{name} type:{message_type}"
-            )
             if message_type == "text":
                 message = messenger.get_message(data)
-                name = messenger.get_name(data)
-                logger.info(message)
-
-                fast_mqtt.publish("whatsapp/get",'{"sender":"' + mobile + '","message":"' + message + '"}')
-                # messenger.send_message(f"Hi {name}, nice to connect with you", mobile)
-
-            elif message_type == "interactive":
-                message_response = messenger.get_interactive_response(data)
-                intractive_type = message_response.get("type")
-                message_id = message_response[intractive_type]["id"]
-                message_text = message_response[intractive_type]["title"]
-                logger.info(f"Interactive Message; {message_id}: {message_text}")
-
-            elif message_type == "location":
-                message_location = messenger.get_location(data)
-                message_latitude = message_location["latitude"]
-                message_longitude = message_location["longitude"]
-                logger.info("Location: %s, %s", message_latitude, message_longitude)
-
-            elif message_type == "image":
-                image = messenger.get_image(data)
-                image_id, mime_type = image["id"], image["mime_type"]
-                image_url = messenger.query_media_url(image_id)
-                image_filename = messenger.download_media(image_url, mime_type)
-                print(f"{mobile} sent image {image_filename}")
-                logger.info(f"{mobile} sent image {image_filename}")
-
-            elif message_type == "video":
-                video = messenger.get_video(data)
-                video_id, mime_type = video["id"], video["mime_type"]
-                video_url = messenger.query_media_url(video_id)
-                video_filename = messenger.download_media(video_url, mime_type)
-                print(f"{mobile} sent video {video_filename}")
-                logger.info(f"{mobile} sent video {video_filename}")
-
-            elif message_type == "audio":
-                audio = messenger.get_audio(data)
-                audio_id, mime_type = audio["id"], audio["mime_type"]
-                audio_url = messenger.query_media_url(audio_id)
-                audio_filename = messenger.download_media(audio_url, mime_type)
-                print(f"{mobile} sent audio {audio_filename}")
-                logger.info(f"{mobile} sent audio {audio_filename}")
-
-            elif message_type == "file":
-                file = messenger.get_file(data)
-                file_id, mime_type = file["id"], file["mime_type"]
-                file_url = messenger.query_media_url(file_id)
-                file_filename = messenger.download_media(file_url, mime_type)
-                print(f"{mobile} sent file {file_filename}")
-                logger.info(f"{mobile} sent file {file_filename}")
-            else:
-                print(f"{mobile} sent {message_type} ")
-                print(data)
+                fast_mqtt.publish("whatsapp/received",'{"sender":"' + mobile + '","message":"' + message + '"}')
         else:
             delivery = messenger.get_delivery(data)
             if delivery:
                 print(f"Message : {delivery}")
-            else:
-                print("No new message")
     return "ok"
 
 
+def send_message(message,recipient,template,language):
+    try:
+        logger.debug("Sending message to: {}".format(recipient))
+        if not template:
+            template=config.get("Whatsapp","api.template_name")
+        if not language:
+            language=config.get("Whatsapp","api.template_language") 
 
-
-
-def send_message(message,recipient):
-    messenger.send_message(message, recipient)
+        if template:
+            msg_body_params = [{"type": "text","text": message}]
+            messenger.send_templatev2(template=template,lang=language,recipient_id=recipient,components=[
+                    {'type': 'body','parameters': msg_body_params}])
+            logger.debug("Message sent using template")
+        else:
+            messenger.send_message(message=message,recipient_id=recipient)
+            logger.debug("Message sent using text message")
+        return "ok"
+    except Exception as e:
+        logger.error("Error sending message to: {}. {}".format(str(recipient),str(e)))
+        return e
     
+
